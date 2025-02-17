06066353cfeefbab1b28bbaa5e961dd53e206c80
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-02-17 15:03:04 +0100

    setup

---
+++ app/index.jsx
@@ -0,0 +1,41 @@
+import { ScreenFooter } from "react-native-screens"
+import { Text, View, Image, ImageBackground, StyleSheet } from 'react-native';
+
+
+export default function App() {
+
+return (
+<ImageBackground source={require('./images/background.jpg')} style={styles.background}>
+<View style={styles.container}>
+    <Text style={styles.title}>extremely cute cat</Text>
+    <Image style={styles.image} source={require('./images/cat.jpg')} />
+</View>
+</ImageBackground>
+);
+
+}
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 24,
+        color: "blue"
+    },
+
+    container: {
+        
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    image: {
+        height: 200,
+        width: 200
+    },
+    background: {
+        height: 740,
+        width: 360
+    }
+})
+
+
+
