fbac85983581100dec805ab7fbb6c2224928d9ea
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-03-06 14:15:35 +0100

    opdracht 2

--- app/screens/auth/register.jsx
+++ app/screens/auth/register.jsx
@@ -1,21 +1,42 @@
-import { View, Text, StyleSheet,Button } from 'react-native'
+import { Text, View, Button, StyleSheet, ImageBackground, TextInput } from 'react-native';
 import { useRouter } from "expo-router";
 
 export default function Register() {
-
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text>register</Text>
-      <Button title="terug" onPress={() => router.back() }/>
-    </View>
-  )
+    <ImageBackground source={require('../../../app/images/RegisterScreen.png')} style={styles.background}>
+      <View style={styles.container}>
+        <TextInput style={styles.input} placeholder="Email Address" placeholderTextColor="black" />
+        <TextInput style={styles.input} placeholder="Password" placeholderTextColor="black" secureTextEntry={true} />
+        <TextInput style={styles.input} placeholder="Confirm Password" placeholderTextColor="black" secureTextEntry={true} />
+
+        <Button color="black" title="CREATE ACCOUNT" onPress={() => router.replace("/screens/tabs/home")} />
+        <Text onPress={() => router.replace("/screens/auth/login")}>Already have an account? Sign in</Text>
+      </View>
+    </ImageBackground>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    borderColor: 'white',
+    borderWidth: 1,
+    backgroundColor: "white",
+    opacity: 0.8,
+    marginBottom: 15,
+    paddingHorizontal: 100,
+  },
+
+  background: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center"
   }
-})
+});
