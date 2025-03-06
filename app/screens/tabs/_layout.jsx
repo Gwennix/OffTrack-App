1c284f20b897cd73c7ab0acf02abd77282039888
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-03-06 14:55:06 +0100

    opdracht 3

--- app/screens/tabs/_layout.jsx
+++ app/screens/tabs/_layout.jsx
@@ -10,10 +10,22 @@ export default function TabsLayout() {
     <Ionicons name='home-outline' size={24} />
   )}} />
 
+   <Tabs.Screen name="search" options={{title: "Search", tabBarIcon: () => (
+    <Ionicons name='search' size={24} />
+  )}} />
+
+    <Tabs.Screen name="new" options={{title: "New", tabBarIcon: () => (
+    <Ionicons name='add' size={24} />
+  )}} />
+
+    <Tabs.Screen name="notifications" options={{title: "Notifications", tabBarIcon: () => (
+    <Ionicons name='notifications-outline' size={24} />
+  )}} />
+
     <Tabs.Screen name="profile" options={{title: "profile", tabBarIcon: () => (
     <Ionicons name='person-outline' size={24} />
   )}} />
+  
    </Tabs>
   )
 }
-
