e45d3cbf0ef03ac92c620fdb991e82e62f8cb9f5
Gwennix <143797512+Gwennix@users.noreply.github.com>
2025-04-11 13:39:10 +0200

    notifs

---
+++ app/notifinfo.js
@@ -0,0 +1,40 @@
+const notifinfo = [
+    {
+        id: 1,
+        image: require("../assets/images/notif1.jpg"),
+        title: "Twilight",
+        content: "BOOOOOOOOOOOKS"
+    },
+    {
+        id: 2,
+        image: require("../assets/images/notif2.png"),
+        title: "Fluttershy",
+        content: "Uhm hi"
+    },
+    {
+        id: 3,
+        image: require("../assets/images/notif3.png"),
+        title: "Pinkie",
+        content: "Get ready for my arrival, Worm!"
+    },
+    {
+        id: 4,
+        image: require("../assets/images/notif4.jpg"),
+        title: "Discord",
+        content: "ik weet waar je bed slaapt"
+    },
+    {
+        id: 5,
+        image: require("../assets/images/notif5.jpg"),
+        title: "Rainbow",
+        content: "20% cooler"
+    },
+    {
+        id: 6,
+        image: require("../assets/images/notif6.jpg"),
+        title: "Luna",
+        content: "Zin in moonshine?"
+    },
+]
+
+export default notifinfo;
--- app/screens/tabs/notifications.jsx
+++ app/screens/tabs/notifications.jsx
@@ -1,18 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
 import React from 'react'
+import notifinfo from '../../notifinfo'
 
 export default function NotificationsScreen() {
   return (
-    <View>
-      <Text>My notifications</Text>
+    <View style={styles.container}>
+      <View style={styles.headerText}>
+      <Text style={styles.title}>🔔 My notifications</Text>
+
+      <FlatList
+        data={notifinfo}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image source={item.image} style={styles.image} />
+            <View style={styles.profile}>
+              <Text >{item.title}</Text>
+              <Text style={styles.content}>{item.content}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F8F8',
+    padding: 15,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 50,
+  },
+  item: {
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: 
+    { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  profile: {
+    marginLeft: 10,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  content: {
+    fontSize: 14,
+    color: '#555',
+  },
+});
